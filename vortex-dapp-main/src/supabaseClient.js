cac5166ee550aa31a88615a6b478e5c36a526079
Vortex Dapp <duarte.baltazar@vortexdapp.com>
2024-10-11 17:09:44 +0100

    ENV changed

--- vortex-dapp-main/src/supabaseClient.js
+++ vortex-dapp-main/src/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Retrieve environment variables
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 // Validate environment variables
 if (!supabaseUrl || !supabaseAnonKey) {
