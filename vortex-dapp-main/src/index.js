6a01e3d1b13bd337e6f1bc226a4ba8ea0b535b78
dcaldeirao1 <duarte.baltazar@vortexdapp.com>
2024-10-28 18:59:51 +0000

    Update index.js

--- vortex-dapp-main/src/index.js
+++ vortex-dapp-main/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+// Check URL parameter to decide which app to load
+const isTelegramApp = window.location.search.includes("mode=telegram");
+
+const MainApp = isTelegramApp
+  ? require("./TelegramApp").default
+  : require("./App").default;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>
 );
 
