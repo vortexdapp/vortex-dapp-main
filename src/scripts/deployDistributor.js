d2c21fb08476f8e7e91bb44c33ad1b7f7f5e0ed6
dcaldeirao1 <duarte.baltazar@vortexdapp.com>
2024-11-06 17:29:30 +0000

    test

--- src/scripts/deployDistributor.js
+++ src/scripts/deployDistributor.js
@@ -1,23 +1,29 @@
 const { ethers, run } = require("hardhat");
 
 async function main() {
+  const uniswapV3Factory_address = process.env.SEPOLIA_UNISWAP_FACTORY;
+  const positionManager_address = process.env.SEPOLIA_POSITION_MANAGER;
+  const swap_router = process.env.SEPOLIA_SWAP_ROUTER;
+  const WETH_address = process.env.SEPOLIA_WETH;
+  const quoter = process.env.SEPOLIA_QUOTER;
+
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const tokenAddress = "0xE3Bb1E884Ee24aA5f0E399416da698Da43a2a476";
+  const ContractFactory = await ethers.getContractFactory("Swap");
 
-  const ContractFactory = await ethers.getContractFactory("RewardDistributor");
+  const contract = await ContractFactory.deploy(
+    positionManager_address,
+    WETH_address,
+    uniswapV3Factory_address,
+    swap_router,
+    quoter
+  );
+  const SwapperAddress = contract.target;
+  console.log("Swapper address:", contract.target);
 
-  // Deploy the contract with custom gas settings for BSC
-  const contract = await ContractFactory.deploy({
-    gasPrice: ethers.parseUnits("5", "gwei"), // Adjust gas price as needed
-  });
-
-  // Wait for deployment to finish
-  //await contract.wait();
-
-  console.log("Distributor Contract deployed to:", contract.target);
+  /* console.log("Distributor Contract deployed to:", contract.target);
   try {
     await run("verify:verify", {
       address: contract.target,
@@ -26,13 +32,7 @@ async function main() {
     console.log("Contract verified on Etherscan");
   } catch (error) {
     console.error("Verification failed:", error);
-  }
-
-  /* const MyFlashCoin = await ethers.getContractFactory("FlashCoin");
-  const MyFlashCoinDeployment = await MyFlashCoin.deploy("Thether USD", "USDT");
-
-  console.log("FlashCoin address:", MyFlashCoinDeployment.target);
-  const flashAddress = MyFlashCoinDeployment.target; */
+  } */
 }
 
 main()
--- src/scripts/interactDistributor.js
+++ src/scripts/interactDistributor.js
@@ -5,71 +5,25 @@ const { parse } = require("csv-parse/sync");
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(
-    "Interacting with the reward distributor contract using the account:",
+    "Interacting with the swap contract using the account:",
     deployer.address
   );
 
   // Total rewards in wei (0.01 ETH) converted to BigInt
-  const totalReward = BigInt(ethers.parseEther("0.01").toString());
+  const amountIn = ethers.parseEther("0.01");
 
-  const distributorAddress = "0x50D0f97D8d64E8403922a49C9Ac42e6Ac8809297";
-  const RewardDistributor = await ethers.getContractFactory(
-    "RewardDistributor"
-  );
-  const distributor = await RewardDistributor.attach(distributorAddress);
+  tokenAddress = "0x94365bBf2E8F026279c22E8D94b534F8b3C58d8b";
 
-  // Read and parse the CSV file
-  const csvData = fs.readFileSync("holders.csv", { encoding: "utf-8" });
-  const records = parse(csvData, {
-    columns: false,
-    skip_empty_lines: true,
-    trim: true,
+  const swapAddress = "0xe0Bc1dd812B54Fe35a3404F405aE9564F65D08cf";
+  const Swapper = await ethers.getContractFactory("Swap");
+  const swapper = await Swapper.attach(swapAddress);
+
+  const swaptx = await swapper.swapWETHforTokens(amountIn, tokenAddress, {
+    value: amountIn,
+    gasLimit: 1000000,
   });
-
-  // Total supply in wei (assuming the total supply is 1 billion tokens)
-  const totalSupply = BigInt(ethers.parseUnits("1000000000", 18).toString());
-
-  let addresses = [];
-  let rewards = [];
-  let balances = [];
-
-  for (let record of records) {
-    let address = record[0]; // First column is the address
-    let balanceString = record[1].replace(/,/g, ""); // Second column is the balance, remove commas
-
-    // Check if the balance string is a valid number before proceeding
-    if (!isNaN(parseFloat(balanceString)) && parseFloat(balanceString) > 0) {
-      // Convert the balance string to BigInt
-      let balance = BigInt(ethers.parseUnits(balanceString, 18).toString());
-
-      // Calculate the reward using BigInt arithmetic
-      let reward = (balance * totalReward) / totalSupply; // BigInt multiplication and division
-
-      console.log("Address: ", address);
-      //console.log("Balance: ", balance.toString());
-      console.log("Reward: ", reward.toString());
-
-      addresses.push(address);
-      balances.push(balance.toString()); // Convert back to BigNumber for smart contract
-
-      // Send the reward directly from the deployer account
-      const tx = await deployer.sendTransaction({
-        to: address,
-        value: reward,
-      });
-
-      await tx.wait(); // Wait for the transaction to be mined
-      //console.log(`Reward sent to ${address}, tx hash: ${tx.hash}`);
-      console.log(`Reward sent to ${address}`);
-    } else {
-      console.error(
-        "Invalid balance data for address",
-        address,
-        "with balance data:",
-        balanceString
-      );
-    }
-  }
+  await swaptx.wait();
+  console.log("Swap done!");
 }
 
 main().catch((error) => {
