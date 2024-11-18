0a428659b8d03ccbc0159322ca42105e44a7870b
Vortex Dapp <duarte.baltazar@vortexdapp.com>
2024-11-18 02:28:54 +0000

    Update TokenListPage.js

--- src/pages/TokenListPage.js
+++ src/pages/TokenListPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import TokenListTable from "../components/TokenListTable.js";
 import TokenList from "../components/TokenList.js";
-import Header2 from "../components/Header2.js";
+import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import "./TokenListPage.css"; // Import the CSS file for TokensPage
 
 function TokensPage() {
   return (
     <div>
-      <Header2 />
+      <Header />
       <div className="token-list-desktop">
         <TokenListTable />
       </div>
